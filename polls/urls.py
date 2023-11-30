293ae578fd21c0f0158846b4839d5f817a732c2f
Criz Franchesca Marie Cruz <mischievous.virus@gmail.com>
2023-11-30 13:55:31 +0800

    setup

--- polls/urls.py
+++ polls/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('index', views.index, name="polls"),
+    path('sample', views.sample, name="template"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello motherfather.")
+
+def sample(request):
+    context = {
+        "user_fullname" : "Chiwa",
+        "greetings" : "Hiiii",
+        "skills" : {"tulog", "kain"}
+    }
+    return render(request, 'template.html', context)
